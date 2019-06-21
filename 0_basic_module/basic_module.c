bc710c3e163a922d8b92f1e504fa12ffd152baab
Przemyslaw Gasior <przemyslaw.m.gasior@gmail.com>
2019-06-21 23:23:24 +0200

    Initial commit

---
+++ 0_basic_module/basic_module.c
@@ -0,0 +1,47 @@
+/**
+ * @file basic_module.c
+ * @author Przemyslaw Gasior
+ * @brief An basic loadable kernel module which displays a message in dmesg (i.e. /var/log/kern.log file).
+ * There is possibility to pass an argument which is also displayed later on.
+ * @version 0.1
+ * @date 2019-06-21
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/* Setup module parameters */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Przemyslaw Gasior");
+MODULE_DESCRIPTION("A basic LKM.");
+MODULE_VERSION("0.1");
+
+/* Setup input argument for kernel module */
+static char *text = "there";  
+module_param(text, charp, S_IRUGO);
+MODULE_PARM_DESC(text, "The text displayed in kernel log");
+
+/**
+ * @brief Init function
+ */
+static int __init basic_module_init(void){
+   printk(KERN_INFO "basic_module: Hello %s from the kernel site!\n", text);
+   return 0;
+}
+
+/**
+ * @brief Exit function
+ */
+static void __exit basic_module_exit(void){
+   printk(KERN_INFO "basic_module: Goodbye %s from the kernel site!\n", text);
+}
+
+/**
+ * @brief Driver initialization entry point 
+ */
+module_init(basic_module_init);
+module_exit(basic_module_exit);
---
+++ README.md
@@ -0,0 +1,15 @@
+# lkm-sandbox
+
+Loadable kernel modules sandbox used to learn about LKMs.
+
+## Prerequisities
+
+
+## Usage
+
+
+## List of modules
+* 0_basic_module
+    *
+
+## Sources
